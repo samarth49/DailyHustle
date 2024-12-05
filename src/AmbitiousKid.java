f92e55def6213ce88e1724b342042b65f5266bb3
samarth49 <otarisamarth49@gmail.com>
2024-12-05 21:50:13 +0530

    CP-31

---
+++ src/AmbitiousKid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AmbitiousKid {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(Math.abs(arr[i]) <= smallest){
+                smallest = Math.abs(arr[i]);
+            }
+        }
+        System.out.println(smallest);
+    }
+}
